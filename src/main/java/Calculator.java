9e761cbd1be71740bdac4ac1d55bce338c68052a
janneesa <janne.savinainen@hotmail.com>
2025-03-20 10:24:40 +0200

    Add multilingual support for fuel cost calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,9 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
 public class Calculator {
-    private double litersPerKm = 5.0;
+    private double litersPerKm = 0.33;
     private double fuelNeeded;
     private double fuelCost;
 
@@ -23,5 +27,51 @@ public class Calculator {
 
     public double getFuelCost() {
         return fuelCost;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        System.out.println("Select a language");
+        System.out.println("1. Farsi");
+        System.out.println("2. Finnish");
+        System.out.println("3. Japanese");
+
+        Locale locale = Locale.getDefault();
+
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                locale = new Locale("fa", "IR");
+                break;
+            case 2:
+                locale = new Locale("fi", "FI");
+                break;
+            case 3:
+                locale = new Locale("ja", "JP");
+                break;
+            default:
+                System.out.println("Invalid choice. Defaulting to English (US)");
+                locale = new Locale("en", "US");
+                break;
+        }
+
+        ResourceBundle rb;
+        try {
+            rb = ResourceBundle.getBundle("messages", locale);
+        } catch (Exception e) {
+            System.out.println("Invalid locale. Defaulting to English (US)");
+            rb = ResourceBundle.getBundle("messages", new Locale("en", "US"));
+        }
+
+        System.out.println(rb.getString("kilo"));
+        double kilo = sc.nextDouble();
+
+        System.out.println(rb.getString("price"));
+        double price = sc.nextDouble();
+
+        double cost = calculator.calculateFuelConsumption(kilo, price);
+        System.out.println(rb.getString("total") + cost);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Calculator calculator = new Calculator();
+
         double km = 100;
         double fuelPricePerLiter = 2.5;
+
         double fuelCost = calculator.calculateFuelConsumption(km, fuelPricePerLiter);
         System.out.println("Fuel cost for " + km + " km: " + fuelCost);
-        calculator.setLitersPerKm(6.0);
+
+        calculator.setLitersPerKm(0.45);
         fuelCost = calculator.calculateFuelConsumption(km, fuelPricePerLiter);
         System.out.println("Fuel cost for " + km + " km: " + fuelCost);
+
+        System.out.println("Select a language");
+        System.out.println("1. Farsi");
+        System.out.println("2. Finnish");
+        System.out.println("3. Japanese");
+
+        int choice = sc.nextInt();
     }
 }
